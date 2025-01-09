43a9739a5b49403125b163191ace25c5a381906d
KKdohyeong <dohyeong254@naver.com>
2025-01-09 15:15:12 +0900

    #3-usecase: gitflow-feature-stash: #6-springboot

---
+++ alarm_service/src/main/java/com/dayspark/alarm_service/AlarmServiceApplication.java
@@ -0,0 +1,13 @@
+ package com.dayspark.alarm_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlarmServiceApplication.class, args);
+    }
+
+}
